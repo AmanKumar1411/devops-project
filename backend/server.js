96767c839b064a158b3605a985f135e143c4a46d
AmanKumar <itsamankumar786@gmail.com>
2026-02-01 23:40:59 +0530

    testing CD

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend" });
+  res.json({ message: "Hello from Backend" });
 });
 
 app.listen(3000, () => {
   console.log("Backend running on port 3000");
-}); 
+});
