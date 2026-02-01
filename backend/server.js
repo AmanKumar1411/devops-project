c5dadef407d5ae54884953878e496b5e4dd7b72b
AmanKumar <itsamankumar786@gmail.com>
2026-02-01 20:28:41 +0530

    testing for v7

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend v6" });
+  res.json({ message: "Hello from backend V1" });
 });
 
 app.listen(3000, () => {
