b6f50d6df0ffc62c31ee29a2f6db48722644e4e1
AmanKumar <itsamankumar786@gmail.com>
2026-02-03 03:42:16 +0530

    feat: Adjust backend API response message and server binding address, and frontend heading, API endpoint, and add fetch error handling.

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Testing CI and CD" });
+  res.json({ message: "Backend working clean start" });
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Backend running on port 3000");
 });
