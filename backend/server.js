88b33ddaf4d542b29f8c31fac657f5ba03f8cc7e
AmanKumar <itsamankumar786@gmail.com>
2026-02-03 01:47:17 +0530

    Remove CI and test workflows and update backend and frontend messages to reflect CI/CD.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Testing CI" });
+  res.json({ message: "Testing CI and CD" });
 });
 
 app.listen(3000, () => {
