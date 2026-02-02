20900c247cbb5a6e598273e17698408e4cef946d
AmanKumar <itsamankumar786@gmail.com>
2026-02-02 16:10:31 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Backend" });
+  res.json({ message: "Testing CD" });
 });
 
 app.listen(3000, () => {
